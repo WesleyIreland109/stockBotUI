47574bb250b39b86f591e69c6388afa8c669fd6d
WesleyIreland109 <wesley.ireland109@live.com>
2026-05-11 11:58:16 -0500

    404 page and routing update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 import Home from './components/Home';
 import NamingChecker from './components/NamingChecker';
 
 function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const search = window.location.search;
+        if (search.startsWith('/?')) {
+            const path = search.slice(2).split('&')[0].replace(/~and~/g, '&');
+            navigate(path);
+        }
+    }, [navigate]);
+
     return (
         <Routes>
             <Route path="/" element={<Home />} />
